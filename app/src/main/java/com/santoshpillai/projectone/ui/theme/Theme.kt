a8487a887e8685b63d502566519a683e8fdbc14b
Santosh Pillai <sapillai@redhat.com>
2021-06-13 19:17:15 +0530

    updated-theme-color
    Signed-off-by: Santosh Pillai <sapillai@redhat.com>

--- app/src/main/java/com/santoshpillai/projectone/ui/theme/Theme.kt
+++ app/src/main/java/com/santoshpillai/projectone/ui/theme/Theme.kt
@@ -13,8 +13,8 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
+    primary = BlueGrey500,
+    primaryVariant = BlueGrey700,
     secondary = Teal200
 
     /* Other default colors to override
